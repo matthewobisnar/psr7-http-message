d600d8e56530040c14981c8e66e077747fead335
matthew bisnar <matthewobisnar@gmail.com>
2020-12-14 04:30:35 +0800

    first commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+
+require __DIR__ . "/vendor/autoload.php";
+
+use http\Message\Uri;
+
+$request = new \http\Message\Request();
+
+$uri = (new Uri("https://usr:pass@d.phalcon.ld:8080/action?par=val#frag"));
+
+var_dump($uri->withQuery("?name=matthew@data=matthew"));
+
